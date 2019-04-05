2d6dd029532c5a14737a63f9f2a806192ed621eb
Juventino Neto <nettojuventino@gmail.com>
2019-04-04 22:07:51 -0300

    Remoção do projeto de frontend devido à problemas

--- API/Cargo.Infrastructure/Program.cs
+++ API/Cargo.Infrastructure/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Cargo.Infrastructure
 {
